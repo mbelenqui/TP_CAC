3e2480c9f6c47e75968ddb63ee4012e2b7701799
mbelenqui <88200092+mbelenqui@users.noreply.github.com>
2022-06-13 13:12:20 -0300

    Update validacion.js

--- TP_CAC/js/validacion.js
+++ TP_CAC/js/validacion.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function validarForm(evento) {
     evento.preventDefault();
     var name = document.getElementById('name').value;
-    var expReg= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/
+    var expReg= /^\w+([\.-_]?\w+)*@\w+([\.-_]?\w+)*(\.\w{2,3,4})+$/
     if(name.length == 0) {
         alert("Por favor ingrese un nombre en el formulario de contacto")
         return;
@@ -20,7 +20,7 @@ function validarForm(evento) {
         return;
     }
     alert('¡Gracias! Su mensaje fue enviado con exito')
-    form.submit();
+    this.submit();
 }
 
    
