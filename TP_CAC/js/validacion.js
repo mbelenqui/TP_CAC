bddb5e808549c86904e3637fc6eea274a62fe82a
mbelenqui <88200092+mbelenqui@users.noreply.github.com>
2022-06-13 13:04:33 -0300

    Update validacion.js

--- TP_CAC/js/validacion.js
+++ TP_CAC/js/validacion.js
@@ -1,27 +1,27 @@
-function validar(){
-    var form = document.form;
-    //Ve que los campos no se envien vacios
-
-    if(form.nombre.value==0){
-        alert("Por favor ingrese un nombre en el formulario de contacto");
-        form.nombre.value="";
-        form.nombre.focus();
-        return false;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form").addEventListener('submit', validarForm); 
+});
+  
+function validarForm(evento) {
+    evento.preventDefault();
+    var name = document.getElementById('name').value;
+    var expReg= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/
+    if(name.length == 0) {
+        alert("Por favor ingrese un nombre en el formulario de contacto")
+        return;
     }
-    if(form.correo.value==0){
-        alert("Por favor ingrese una direccion de mail en el formulario de contacto");
-        form.correo.value="";
-        form.correo.focus();
-        return false;
-    }
-
-    
-    if(form.mensaje.value==0){
+    var email = document.getElementById('email').value;
+    if (expReg.test(email)==false){
+         alert("Por favor ingrese una direccion de mail valida")
+       }
+    var mensaje = document.getElementById('mensaje').value;
+    if(mensaje.length==0) {
         alert("El campo mensaje esta vacio");
-        form.mensaje.value="";
-        form.mensaje.focus();
-        return false;
+        return;
     }
-    alert("¡Gracias!El mensaje se envio con exito")
+    alert('¡Gracias! Su mensaje fue enviado con exito')
     form.submit();
 }
+
+   
+
